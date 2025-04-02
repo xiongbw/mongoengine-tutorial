810e79a6b279cc28055746a9d37e3992875c05de
xiongbw <beartheboss@163.com>
2025-04-02 20:43:32 +0800

    config: Example settings for MongoDB Replica Set Server

--- config/settings.py
+++ config/settings.py
@@ -8,3 +8,12 @@ MONGODB_SETTINGS = {
     'host': 'localhost',
     'port': 27017
 }
+
+"""
+Settings for MongoDB Replica Set Server with URI string
+https://docs.mongoengine.org/guide/connecting.html#connect-with-uri-string
+
+MONGODB_SETTINGS = {
+    'host': 'mongodb://localhost:37017,localhost:37018,localhost:37019/db_charge?replicaSet=rs0',
+}
+"""
