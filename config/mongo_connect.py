45540da22b41cc2ee0b603ae418f16ef1ee73a1c
xiongbw <beartheboss@163.com>
2025-04-12 11:41:33 +0800

    config: PyMongo Logger

--- config/mongo_connect.py
+++ config/mongo_connect.py
@@ -1,4 +1,7 @@
+import logging
+
 from mongoengine import connect
+
 from config.settings import MONGODB_SETTINGS
 
 """
@@ -7,3 +10,7 @@ https://docs.mongoengine.org/guide/connecting.html
 """
 
 connect(**MONGODB_SETTINGS)
+
+# Config pymongo logger
+pymongo_logger = logging.getLogger('pymongo')
+pymongo_logger.setLevel(logging.DEBUG)
