dd56440aa82928d78d14e2dc5c4b71ca283e9def
xiongbw <beartheboss@163.com>
2025-04-12 11:49:33 +0800

    tests: Add some simple demos that query in MongoDB

---
+++ services/order_service.py
@@ -0,0 +1,69 @@
+from decimal import Decimal
+from datetime import datetime
+from typing import List
+
+from bson import ObjectId
+
+from config import mongo_connect
+from constants.order_status_enums import OrderStatusEnum
+from models.order import Order, OrderLine
+
+
+def create_one(city: str, country: str, name: str, phone: str, shipping_fee: Decimal, state: str, street: str,
+               total: Decimal, user_id: int, zip: str, order_lines: List[OrderLine]) -> Order:
+    """
+    创建订单
+    :param city: 城市
+    :param country: 国家
+    :param name: 名称
+    :param phone: 电话号码
+    :param shipping_fee: 邮费
+    :param state: 州
+    :param street: 街道
+    :param total: 总金额
+    :param user_id: 用户 ID
+    :param zip: 邮政编码
+    :param order_lines: 订单详情
+    :return: Order object
+    """
+    now = datetime.now()
+    order = Order(city, country, now, False, name, now, phone, shipping_fee, state,
+                  OrderStatusEnum.CREATED.value, street, total, now, user_id, zip, order_lines)
+    return order.save()
+
+
+def find_one(order_id: Order.id):
+    """
+    获取订单
+    :param order_id: 订单 ID
+    :return: Order object
+    """
+    if not ObjectId.is_valid(order_id):
+        return None
+
+    return Order.objects(id=order_id, is_deleted=False).first()
+
+
+def count_orders(country: str = None, city: str = None) -> int:
+    """
+    统计订单数量
+    :param country: 国家
+    :param city: 城市
+    :return: Order count
+    """
+    query_filter = {'is_deleted': False}
+    if country and country.strip():
+        query_filter['country'] = country
+    if city and city.strip():
+        query_filter['city'] = city
+
+    return Order.objects(**query_filter).count()
+
+
+def count_user_orders(user_id: int) -> int:
+    """
+    统计用户订单数量
+    :param user_id: 用户 ID
+    :return: Order count
+    """
+    return Order.objects(user_id=user_id, is_deleted=False).count()
---
+++ tests/test_order_service.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from decimal import Decimal
+import unittest
+
+from models.order import OrderLine
+from services import order_service
+
+
+class TestOrderService(unittest.TestCase):
+
+    def test_add_one(self):
+        city = "Klington"
+        country = "Belgium"
+        name = "Cathryn Langosh"
+        phone = "(216) 394-6317"
+        shipping_fee = Decimal("5")
+        state = "Colorado"
+        street = "1674 Schumm Camp"
+        total = Decimal("435")
+        user_id = 1031
+        zip = "37146"
+
+        order_line1 = {
+            "product": "Intelligent Concrete Computer", "sku": "2715", "qty": 92,
+            "price": Decimal("90.00"), "cost": Decimal("72.9")
+        }
+        order_line2 = {
+            "product": "Incredible Wooden Chair", "sku": "202", "qty": 45,
+            "price": Decimal("77.00"), "cost": Decimal("70.84")
+        }
+        order_lines = [OrderLine(**order_line1), OrderLine(**order_line2)]
+        order = order_service.create_one(city, country, name, phone, shipping_fee, state, street, total, user_id, zip,
+                                         order_lines)
+        print(order)
+
+    def test_find_one(self):
+        order_id = "67f0e8dfb889cbfa4377388c"
+        order = order_service.find_one(order_id)
+        print(order)
+
+    def test_count_orders(self):
+        country = "China"
+        city = "Shenzhen"
+        count = order_service.count_orders(country, city)
+        print(f"In {country} {city} has {count} orders")
+
+    def test_count_user_orders(self):
+        user_id = 123
+        count = order_service.count_user_orders(user_id)
+        print(f"User {user_id} has {count} orders")
+
+
+if __name__ == '__main__':
+    unittest.main()
