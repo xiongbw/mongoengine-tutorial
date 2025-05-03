5e56768ca3f557d982cc7a8ca3c01eef3837ce36
xiongbw <beartheboss@163.com>
2025-05-03 19:37:31 +0800

    feat: Function for hiring an employee

---
+++ services/employee_service.py
@@ -0,0 +1,14 @@
+from models.employee import Employee
+
+
+def hire_one(department: str, name: str, gender: str) -> Employee:
+    """
+    录用员工
+    :param name: the employee name
+    :param gender: the employee gender
+    :param department: the employee department
+    :return: Employee object
+    """
+    employee = Employee(name=name, gender=gender, department=department)
+    return employee.save()
+
