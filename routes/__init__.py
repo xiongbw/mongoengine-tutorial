a1e50690b2c609d37ea577cd9f2b440a00e06982
xiongbw <beartheboss@163.com>
2025-05-03 20:03:54 +0800

    config: Route for '/order/*' API

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
 from .employee_routes import bp_employee
+from .order_routes import bp_order
 
 """
 API routes
 """
-all_blueprints = [bp_employee]
+all_blueprints = [bp_employee, bp_order]
---
+++ routes/order_routes.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+bp_order = Blueprint('order', __name__)
+
+__API_PREFIX__ = "/order"
